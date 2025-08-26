c0086c51b649e1b8b3b77d00ae8918d09c89c660
eric-14 <ericmwas01@gmail.com>
2025-08-26 07:50:12 +0300

    Sieve of Eratosthenes

---
+++ sieve.ml
@@ -0,0 +1,23 @@
+
+(* Sieve of Eratosthenes*)
+let max = read_int()
+
+let prime = Array.make (max + 1) true 
+
+let () =
+    prime.(0) <- false;
+    prime.(1) <- false;
+
+    let limit = truncate (sqrt (float max)) in 
+    for n = 2 to limit do 
+      let m = ref (n * n) in 
+      while !m <= max do 
+          prime.(!m) <- false; 
+          m := !m + n 
+      done 
+    done 
+
+let () = 
+    for i =0 to max do 
+        if prime.(i) then Printf.printf "prime number -> %d\n " i 
+    done 
